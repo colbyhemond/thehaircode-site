3575644f2309ede429d870eba9c8e1049c345134
Colby Hemond <hemond.colby@gmail.com>
2020-10-19 07:51:10 -0400

    update admin for git-gateway

--- pages/order.js
+++ pages/order.js
@@ -16,17 +16,10 @@ export default class Order extends Component {
           </Head>
           <div className="container">
             <h1>{title}</h1>
-            <div className="d-flex justify-content-center">
-              <div className="row">
-                <div className="col-md-6">
-                  <OrderForm />
-                </div>
-                <div className="col-md-6">
-                  <iframe src="https://calendar.google.com/calendar/embed?src=7eo8cbdt5h82dfrujv2omptnhc%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-                </div>
-              </div>
+            <div className="d-flex justify-content-center"> 
+              <OrderForm />
             </div>
-
+            <iframe src="https://calendar.google.com/calendar/embed?src=7eo8cbdt5h82dfrujv2omptnhc%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
           </div>
 
 
