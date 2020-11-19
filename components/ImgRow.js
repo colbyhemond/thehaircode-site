298a106c14fbb675b028b696cba08b5e6947393d
Colby Hemond <hemond.colby@gmail.com>
2020-11-19 10:50:49 -0500

    ImgRown style, Button style, body font Radley

--- components/ImgRow.js
+++ components/ImgRow.js
@@ -1,7 +1,7 @@
 import styles from "./imgrow.module.css";
 
 const ImgRow = props => (
-    <div className="container row d-flex align-items-center flex-wrap">
+    <div className="container row d-flex align-items-center flex-wrap pt-5 mt-5">
         <div className="col-md-4 d-flex justify-content-center">
             <img className={styles.img} src={props.img1}></img>
         </div>
