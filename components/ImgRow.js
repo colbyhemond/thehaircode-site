77f7458093c67263038cdf788e48ce0cd04c411e
Colby Hemond <hemond.colby@gmail.com>
2020-11-21 09:59:25 -0500

    added favicon

--- components/ImgRow.js
+++ components/ImgRow.js
@@ -1,14 +1,14 @@
 import styles from "./imgrow.module.css";
 
 const ImgRow = props => (
-    <div className="container row d-flex align-items-center flex-wrap pt-5 mt-5">
-        <div className="col-md-4 d-flex justify-content-center">
+    <div className="container d-flex flex-wrap pt-5 mt-5">
+        <div className="col-md-4 d-flex justify-content-center my-1">
             <img className={styles.img} src={props.img1}></img>
         </div>
-        <div className="col-md-4 d-flex justify-content-center">
+        <div className="col-md-4 d-flex justify-content-center my-1">
             <img className={styles.img} src={props.img2}></img>
         </div>
-        <div className="col-md-4 d-flex justify-content-center">
+        <div className="col-md-4 d-flex justify-content-center my-1">
             <img className={styles.img} src={props.img3}></img>
         </div>
     </div>
--- components/Layout.js
+++ components/Layout.js
@@ -13,28 +13,28 @@ export default function Layout({ children, home, title }) {
     return (
         <>
         <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous"/>
-        <meta
-          name="description"
-          content="The Blended Bakery."
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-    <title>{title} - The Blended Bakery</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous" />
+          <meta name="description" content="The Blended Bakery." />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta property="og:image" content={`https://og-image.now.sh/${encodeURI( siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+          <title>{title} - The Blended Bakery</title>
         </Head>
-        <NavBar navButtons={navButtons} className='body'/>
+        <NavBar navButtons={navButtons} className='body' />
 
-      <main className={styles.main}>{children}</main>
+        <main className={styles.main}>{children}</main>
 
-      <Footer navButtons={navButtons} className='body'/>
+        <Footer navButtons={navButtons} className='body' />
         </>
      )
 }
